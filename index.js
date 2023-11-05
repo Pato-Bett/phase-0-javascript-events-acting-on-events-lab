dcd13510f3aca321cf3151a6c7fa28c785e06a5e
Pato-Bett <patobett588@gmail.com>
2023-11-06 01:52:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+dodger.style.background = "#FF69B4"
+dodger.style.bottom = "100px"
+
+// document.addEventListener("keydown", function (event) {
+//     // console.log(event);
+//     if (event.key === "ArrowLeft"){
+//         const leftNumbers = dodger.style.left.replace("px", "")
+//         const left = parseInt(leftNumbers, 10)
+//         dodger.style.left = left-1 + "px"
+//     }
+//   });
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+// function to move dodger right
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left<360) {
+        dodger.style.left = left+1 + "px"
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if (key = "ArrowRight") {
+        moveDodgerRight()
+    }
+})
